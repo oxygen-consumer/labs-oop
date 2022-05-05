333740e1fe883be203beda3f94433c18f5b9c706
Zaharie Stefan-Florin <stefan@zaha.tech>
2022-05-05 15:35:42 +0300

    Admin pannel layout

--- Lab 9/Presentation/admin_panel.cpp
+++ Lab 9/Presentation/admin_panel.cpp
@@ -3,3 +3,117 @@
 //
 
 #include "admin_panel.h"
+
+void admin_panel::print_menu() {
+    std::cout << "=== Admin Panel ===" << std::endl;
+    std::cout << "1. List products" << std::endl;
+    std::cout << "2. Add product" << std::endl;
+    std::cout << "3. Remove product" << std::endl;
+    std::cout << "4. Edit product" << std::endl;
+    std::cout << "5. List banknotes" << std::endl;
+    std::cout << "6. Show accepted bankote values" << std::endl;
+    std::cout << "7. Add banknote" << std::endl;
+    std::cout << "8. Remove banknote" << std::endl;
+    std::cout << "9. Edit banknote" << std::endl;
+    std::cout << "10. Exit" << std::endl;
+}
+
+void admin_panel::run() {
+    bool running = true;
+
+    while (running) {
+        print_menu();
+
+        std::cout << "> ";
+        std::string input;
+        int choice;
+        std::cin >> input;
+        try {
+            choice = std::stoi(input);
+        } catch (...) {
+            std::cout << "Invalid input" << std::endl;
+            continue;
+        }
+
+        switch (choice) {
+            case 1: {
+                list_products();
+                break;
+            }
+            case 2: {
+                add_product();
+                break;
+            }
+            case 3: {
+                remove_product();
+                break;
+            }
+            case 4: {
+                edit_product();
+                break;
+            }
+            case 5: {
+                list_banknotes();
+                break;
+            }
+            case 6: {
+                show_accepted_banknote_values();
+                break;
+            }
+            case 7: {
+                add_banknote();
+                break;
+            }
+            case 8: {
+                remove_banknote();
+                break;
+            }
+            case 9: {
+                edit_banknote();
+                break;
+            }
+            case 10: {
+                running = false;
+                break;
+            }
+        }
+    }
+}
+
+// TODO: Implement the following functions after implementing controller
+
+void admin_panel::list_products() {
+
+}
+
+void admin_panel::list_banknotes() {
+
+}
+
+void admin_panel::add_product() {
+
+}
+
+void admin_panel::add_banknote() {
+
+}
+
+void admin_panel::remove_product() {
+
+}
+
+void admin_panel::remove_banknote() {
+
+}
+
+void admin_panel::edit_product() {
+
+}
+
+void admin_panel::edit_banknote() {
+
+}
+
+void admin_panel::show_accepted_banknote_values() {
+
+}
--- Lab 9/Presentation/admin_panel.h
+++ Lab 9/Presentation/admin_panel.h
@@ -2,12 +2,43 @@
 // Created by zaha on 5/5/22.
 //
 
+#include <iostream>
+#include "../Service/vending_machine_controller.h"
+
 #ifndef LAB_9_ADMIN_PANEL_H
 #define LAB_9_ADMIN_PANEL_H
 
 
 class admin_panel {
+private:
+    vending_machine_controller controller;
 
+    void print_menu();
+
+    void list_products();
+
+    void list_banknotes();
+
+    void add_product();
+
+    void add_banknote();
+
+    void remove_product();
+
+    void remove_banknote();
+
+    void edit_product();
+
+    void edit_banknote();
+
+    void show_accepted_banknote_values();
+
+public:
+    admin_panel() = default;
+
+    ~admin_panel() = default;
+
+    void run();
 };
 
 
